61bad9212243275407fe4595966f679b204d12a1
Ram <ram.mohan@refixd.com>
2019-03-17 21:20:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+var map, infoWindow;
+var x=document.getElementById("map");
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 6
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            console.log(position.coords.latitude);
+            console.log(position.coords.longitude);
+            displayLocation(position.coords.latitude,position.coords.longitude)
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
+function displayLocation(latitude, longitude) {
+    var geocoder;
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(latitude, longitude);
+
+    geocoder.geocode({
+            'latLng': latlng
+        },
+        function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[0]) {
+                    var add = results[0].formatted_address;
+                    var value = add.split(",");
+
+                    count = value.length;
+                    country = value[count - 1];
+                    state = value[count - 2];
+                    city = value[count - 3];
+                    console.log(city);
+                    // x.innerHTML = "city name is: " + city;
+                } else {
+                    // x.innerHTML = "address not found";
+                }
+            } else {
+                // x.innerHTML = "Geocoder failed due to: " + status;
+            }
+        }
+    );
+}
